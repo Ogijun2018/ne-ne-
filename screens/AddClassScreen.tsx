00bb2c1ffa512194b9e35e44a6b0de8808233c8b
Ogijun2018 <o2030028@edu.cc.uec.ac.jp>
2021-02-02 03:35:53 +0900

    新規科目追加画面UI修正

--- screens/AddClassScreen.tsx
+++ screens/AddClassScreen.tsx
@@ -1,7 +1,12 @@
-import * as WebBrowser from 'expo-web-browser';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, TouchableOpacity, Text, View, Button } from 'react-native';
-import TextInput from '../components/atoms/TextInput';
+import {
+  StyleSheet,
+  View,
+  Button,
+  FlatList,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import { setNewCollection, getClasses } from '../lib/firebase';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
@@ -9,68 +14,100 @@ import 'firebase/auth';
 import { KeyboardAwareSectionList } from 'react-native-keyboard-aware-scroll-view';
 
 export default function AddClassScreen({ navigation }) {
-  const [classes, setClasses] = useState('');
-  const [classRoomId, setClassRoomId] = useState('');
+  const days = [
+    '日曜日',
+    '月曜日',
+    '火曜日',
+    '水曜日',
+    '木曜日',
+    '金曜日',
+    '土曜日',
+  ];
+  const [classes, setClasses] = useState();
   const getClasses = async () => {
     // 電気通信大学のところは自分がプロフィールに設定している大学名が入る
     // テスト環境として変更する
     const db = await firebase.database();
-    const ref = db.ref('/classes/電気通信大学/');
-    // Attach an asynchronous callback to read the data at our posts reference
-    ref.once(
-      'value',
-      (snapshot) => {
-        const classes = [];
-        const keys = [];
-        snapshot.forEach((item) => {
-          const itemVal = item.val();
-          keys.push(itemVal);
+    const ref = db.ref('/classes/');
+    ref.once('value', (snapshot) => {
+      const list = [];
+      snapshot.forEach((child) => {
+        list.push({
+          key: child.key,
+          name: child.val().name,
+          roomId: child.val().roomId,
+          schoolName: child.val().schoolName,
+          day: child.val().day,
+          time: child.val().time,
         });
-        for (let i = 0; i < keys.length; i++) {
-          console.log(keys[i]);
-        }
-        // snapshot.forEach((ObjectPerDay) => {
-        //   ObjectPerDay.forEach((ObjectPerTime) => {
-        //     ObjectPerTime.forEach((ObjectPerClass) => {
-        //       const sample = ObjectPerClass.val();
-        //       console.log(sample.name, sample.roomId);
-        //       setClasses(sample.name);
-        //       setClassRoomId(sample.roomId);
-        //       console.log('=======');
-        //     });
-        //   });
-        // });
-      },
-      // function (errorObject) {
-      //   console.log('The read failed: ' + errorObject.code);
-      // },
-    );
+      });
+      setClasses(list);
+    });
   };
   useEffect(() => {
-    // console.log('useEffect');
     getClasses();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>AddClassScreen</Text>
-      {/* {classNames.map((data) => {
-                return <Text>{data}</Text>;
-            })}
-            {classRoomIds.map((data) => {
-                return <Text>{data}</Text>;
-            })} */}
-      {/* {classes.name.map((item) => (
-                <Text>{item}</Text>
-            ))} */}
-      <Text>{classes}</Text>
-      <Text>{classRoomId}</Text>
-      <Button
-        title="新しい授業を追加する"
+      <FlatList
+        style={{ width: '100%' }}
+        data={classes}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              style={{
+                borderWidth: 1,
+                borderRadius: 5,
+                borderColor: '#33A3F2',
+                backgroundColor: '#2f95dc',
+                height: 50,
+                justifyContent: 'center',
+                paddingLeft: 10,
+                marginVertical: 5,
+                marginHorizontal: 20,
+              }}
+              onPress={() => {
+                console.log('aaa');
+              }}
+            >
+              <Text
+                style={{ fontSize: 18, color: 'white', fontWeight: 'bold' }}
+              >
+                {item.schoolName} {days[item.day]} {item.time}限 {item.name}
+              </Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <TouchableOpacity
         onPress={() => {
           navigation.navigate('RegistClassScreen');
         }}
-      />
+        style={{
+          position: 'absolute',
+          right: 30,
+          bottom: 20,
+          width: 60,
+          height: 60,
+        }}
+      >
+        <View
+          style={{
+            borderRadius: 100,
+            width: 60,
+            height: 60,
+            backgroundColor: '#89D9F9',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 30 }}>
+            ＋
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
